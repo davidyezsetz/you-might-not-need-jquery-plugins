5ebbbf6c70dac5697f9f72be43bcff3726b4bf56
David Setz <ds@interactive-pioneers.de>
2015-04-27 13:19:18 +0200

    Add lists.js to filter plugins

---
+++ scripts/main.js
@@ -0,0 +1,11 @@
+/* global List */
+(function(List) {
+  /* jshint unused: false */
+  'use strict';
+
+  var options = {
+    valueNames: [ 'name', 'categories' ]
+  };
+
+  var userList = new List('js_plugins', options);
+})(List);
